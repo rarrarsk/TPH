57948a2c22afc67f1611add3e0b61f5ba3ab592e
rarrarsk <125333263+rarrarsk@users.noreply.github.com>
2023-02-16 16:12:24 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6142809779:AAH9wKCjuZVAvG1MNEFOYk22xLHHvZoBRi8")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 8233223))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "dacf95e444cf26fb3d7e9b997e3ee456")
